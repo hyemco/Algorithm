e120a9f8fa11114f41bd10789e3a72a9021bd775
hyemco <hyemco@gmail.com>
2023-02-03 21:45:20 +0900

    기본수학2 - 골드바흐의 추측

--- BOJ/lv8/9020_골드바흐의 추측.py
+++ BOJ/lv8/9020_골드바흐의 추측.py
@@ -1,6 +1,6 @@
-def prime_number(n):
-    for j in range(2, int(n**0.5) + 1):
-        if n % j == 0:
+def prime_number(a):
+    for j in range(2, int(a**0.5) + 1):
+        if a % j == 0:
             return 1
     return 0
 
@@ -11,29 +11,7 @@ for _ in range(t):
     ans = []
     for i in range(n // 2, n + 1):
         if prime_number(i) == 0 and prime_number((n - i)) == 0:
-
+            ans.append(i)
+            ans.append(n - i)
             break
-
-
-
-    # n 까지의 소수 넣기
-    # prime_num = []
-    # for i in range(2, n + 1):
-    #     if prime_number(i) == 0:
-    #         prime_num.append(i)
-    # # 짝 찾기
-    # temp = []
-    # for num in prime_num:
-    #     mid = int(len(prime_num) / 2)
-    #     while True:
-    #         if (n - prime_num[mid]) in prime_num:
-    #             temp.append(n - prime_num[mid])
-    #             temp.append(prime_num[mid])
-    #             break
-    #         else:
-    #             mid -= 1
-    # temp = sorted(list(set(temp)))
-    # if len(temp) == 1:
-    #     print(temp[0], temp[0])
-    # else:
-    #     print(temp[0], temp[1])
+    print(*sorted(ans))
