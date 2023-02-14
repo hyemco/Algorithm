8989b3558c48ed100b2f43474b236dc6d82b8c62
hyemco <hyemco@gmail.com>
2023-02-15 00:54:34 +0900

    Stack2 - 미로

---
+++ SWEA/Stack/4875_미로.py
@@ -0,0 +1,24 @@
+t = int(input())
+for tc in range(1, t + 1):
+    n = int(input())
+    maze = [list(map(int, list(input()))) for _ in range(n)]
+    di = [0, 1, 0, -1]
+    dj = [1, 0, -1, 0]
+    for i in range(n):
+        for j in range(n):
+            if maze[i][j] == 2:
+                stack = [(i, j)]
+    result = 0
+    while stack:
+        i, j = stack.pop()
+        maze[i][j] = 1
+        for k in range(4):
+            ni = i + di[k]
+            nj = j + dj[k]
+            if 0 <= ni < n and 0 <= nj < n:
+                if maze[ni][nj] == 3:
+                    result = 1
+                elif maze[ni][nj] == 0:
+                    stack.append((ni, nj))
+
+    print(f'#{tc} {result}')
