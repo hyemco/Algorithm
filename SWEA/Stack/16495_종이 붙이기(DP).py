29a45eea0379dd356d95a971cdbbd73b3bc9108f
hyemco <hyemco@gmail.com>
2023-02-14 17:17:24 +0900

    Stack2 - 종이 붙이기(DP)

--- SWEA/Stack/16495_종이 붙이기(DP).py
+++ SWEA/Stack/16495_종이 붙이기(DP).py
@@ -1,11 +1,11 @@
-def paper(N):
-    n = N // 10
-    f = [0] * (n + 1)
+def paper(b):
+    b //= 10
+    f = [0] * (b + 2)
     f[1] = 1
     f[2] = 3
-    for i in range(3, n + 1):
-        f[i] = f[i - 1] + f[i - 2] * 2
-    return f[n]
+    for i in range(3, b + 1):
+        f[i] = f[b - 1] + f[b - 2] * 2
+    return f[b]
 
 
 t = int(input())
