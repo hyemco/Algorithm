649f19bd29ac883e8e52c0312615e7bdd3116a48
hyemco <hyemco@gmail.com>
2023-02-20 14:26:07 +0900

    [D3] Queue - 암호생성기

--- SWEA/Queue/1225_암호생성기.py
+++ SWEA/Queue/1225_암호생성기.py
@@ -1,16 +1,30 @@
-T = 10
-for t in range(1, T+1):
+def enqueue(data):
+    global rear
+    queue[rear] = data
+    rear = (rear + 1) % 8
+
+
+def dequeue():
+    global front
+    front = (front + 1) % 8
+    return queue[front - 1]
+
+
+for _ in range(1, 11):
     tc = int(input())
     queue = list(map(int, input().split()))
 
     i = 1
+    front, rear, cnt = 0, 0, 0
     while True:
         if i > 5:
             i = 1
-        t = queue.pop(0) - i
-        if t <= 0:
-            queue.append(0)
+        num = dequeue() - i
+        if num <= 0:
+            enqueue(0)
+            rear += 1
             break
-        queue.append(t)
+        enqueue(num)
         i += 1
-    print(f'#{tc}', *queue)
+        cnt += 1
+    print(f'#{tc}', *queue[(cnt + 1) % 8:8], *queue[0:(cnt + 1) % 8])
